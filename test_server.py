fce3509d98f0a8396f22af1549a5d215ac46b820
Daniel Taub <daniel.taub@ginkgobioworks.com>
2012-01-30 18:06:22 -0500

    added submodule for now and other fiels

--- test_server.py
+++ test_server.py
@@ -13,11 +13,12 @@ Copyright (c) 2012 CEMMI. All rights reserved.
 import colormap;
 import time as clock;
 from numpy import shape,zeros,minimum,maximum,ravel,array;
-import threading, socket, re, struct, hashlib, json, sys, colorsys, random,webbrowser
+import threading, socket, re, struct, hashlib, json, sys
+import optparse, colorsys, random,webbrowser
 from itertools import izip
 from SocketServer import ThreadingMixIn
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-
+from websocket import WebSocketServer
 
 queue = []
 queueLock = threading.Lock()
@@ -82,79 +83,57 @@ class Screen:
 SCREEN=Screen()
 print SCREEN.pixels
 
-class WebsocketRenderer():
+class WebSocketRenderer(WebSocketServer):
+    """
+    WebSockets server that echos back whatever is received from the
+    client.  
+    """
+    buffer_size = 8096
+
+    def new_client(self):
+        """
+        Echo back whatever is received.
+        """
+
+        cqueue = []
+        c_pend = 0
+        cpartial = ""
+        rlist = [self.client]
+	print self.client
+        while True:
+            wlist = []
+
+            if cqueue or c_pend: wlist.append(self.client)
+            ins, outs, excepts = select.select(rlist, wlist, [], 1)
+            if excepts: raise Exception("Socket exception")
+
+            if self.client in outs:
+                # Send queued target data to the client
+                c_pend = self.send_frames(cqueue)
+                cqueue = []
+
+            if self.client in ins:
+                # Receive client data, decode it, and send it back
+                frames, closed = self.recv_frames()
+                cqueue.extend(["You said: " + f for f in frames])
+
+                if closed:
+                    self.send_close()
+                    raise self.EClose(closed)
+
+
+class RenderThread():
     """Renders frame data over a websocket.
-        Specify: Hostname -- the hostname of the webserver providing the containing page
-        Page: Static page to serve (if using builtin webserver)
-        SourcePort: Port from which static page is serverd
         Port: Websocket listen port
     """
-    
-    def __init__(self,Hostname,Page,SourcePort,Port):
-        self.hostname =Hostname
-        self.port = int(Port)
-        
-        if SourcePort != None:
-            self.orig_port = int(SourcePort)
-        else:
-            self.orig_port = 8080
-        
-        self.clients = []
-        self.clients_lock = threading.Lock()
-        
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.bind(('', self.port))
-        self.sock.listen(1)
-        
+    def __init__(self,opts):
+        self.opts = opts
         self.connection_thread = threading.Thread(target=self.handle_connections)
         self.connection_thread.daemon = True
         self.connection_thread.start()
-            
+
     def handle_connections(self):
-        while True:
-            client, addr = self.sock.accept()
-
-            print 'Accepted websocket connection from %s' % str(addr)
-            header = ''
-            while not re.search("\r?\n\r?\n.{8}", header): # Receive headers + 8 bytes data
-                header += client.recv(1024)
-                print header
-            
-            key1 = re.search("Sec-WebSocket-Key1: (.*)$", header, re.M).group(1)
-            key2 = re.search("Sec-WebSocket-Key2: (.*)$", header, re.M).group(1)
-
-            data = header[-8:]
-
-            key1n = int(re.sub("[^\d]", '', key1))
-            key1ns = key1.count(' ')
-            n1 = key1n // key1ns
-
-            key2n = int(re.sub("[^\d]", '', key2))
-            key2ns = key2.count(' ')
-            n2 = key2n // key2ns
-
-            s = struct.pack("!II", n1, n2) + data
-            respkey = hashlib.md5(s).digest()
-            print respkey 
-            if self.orig_port == 80:
-                origin = 'http://'+self.hostname
-            else:
-                origin = 'http://'+self.hostname+':'+str(self.orig_port)
-            
-            resp = \
-                "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" + \
-                "Upgrade: WebSocket\r\n" + \
-                "Connection: Upgrade\r\n" + \
-                "Sec-WebSocket-Origin:"+ origin + "\r\n" + \
-                "Sec-WebSocket-Location: ws://"+self.hostname+":"+ \
-                    str(self.port)+"/\r\n" + \
-                "Sec-WebSocket-Protocol: ledweb\r\n\r\n" + \
-                respkey + "\r\n"
-            client.send(resp)
-            self.clients_lock.acquire()
-            self.clients.append(client)
-            self.clients_lock.release()
+        self.server = WebSocketRenderer(**self.opts.__dict__)
     
     def render(self, lightSystem, currentTime=clock.time()*1000):
         json_frame = [0]*len(lightSystem)
@@ -170,16 +149,17 @@ class WebsocketRenderer():
         size = SCREEN.size
         
         json_data = json.dumps(dict(status='ok', size=map(int, size), frame=json_frame))
-        self.client_push(json_data)
+#        self.client_push(json_data)
+# ************* here we do server-side push!
         
     def client_push(self, data):
-        self.clients_lock.acquire()
         dead_clients = []
         for i in range(len(self.clients)):
             try:
-                self.clients[i].send("\x00")
-                self.clients[i].send(data)
-                self.clients[i].send("\xff")
+                self.server.send_frames(data)
+                #clients[i].send("\x00")
+                #self.clients[i].send(data)
+                #self.clients[i].send("\xff")
             except socket.error:
                 dead_clients.append(i)
         
@@ -188,51 +168,6 @@ class WebsocketRenderer():
             del self.clients[dead_clients[i]-i]
         self.clients_lock.release()
     
-    def close_sock(self, s):
-        try:
-            c.shutdown(socket.SHUT_RDWR)
-            c.close()
-        except Exception:
-            pass
-
-
-class WebHandler(object):
-    def GET(self, action):
-        if action == '':
-            action = 'index.html'
-
-        if not action.startswith('input/'):
-            raise web.Found('/static/'+action)
-
-        params = web.input()
-        queueLock.acquire()
-        queue.append((action, params))
-        queueLock.release()
-
-        return json.dumps(dict(success=True))
-
-#this is unused
-class Webserver: 
-    def __init__(self,hostname,port):
-        urls = ('/(.*)', 'WebHandler')
-        env = globals()
-        env['sys'].argv = []
-        app = web.application(urls, env)
-
-        server_thread = threading.Thread(target=app.run)
-        server_thread.daemon = True
-        server_thread.start()
-        # 
-        #     def sensingLoop(self):
-        #         queueLock.acquire()
-        #         inputs = []
-        #         while len(queue) > 0:
-        #             inputs.append(queue.pop())
-        #         resp = dict(inputs=inputs)
-        #         queueLock.release()
-        # #        self.respond(resp)
-        # 
-
 
 class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
     pass
@@ -276,7 +211,7 @@ class MyHandler(BaseHTTPRequestHandler):
         hwrite=self.hwrite
         try:
             if self.path.startswith("/static/"):
-		f = open('.'+self.path,"r")
+		f = open(self.path[8:],"r")
                 self.send_response(200)
                 self.send_header('Content-type','text/html; charset=utf-8')
                 self.end_headers() 
@@ -311,10 +246,29 @@ class MyHandler(BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    
+  
+    parser = optparse.OptionParser(usage="%prog [options] [listen_port]")
+    parser.add_option("--verbose", "-v", action="store_true",
+            help="verbose messages and per frame traffic")
+    parser.add_option("--cert", default="self.pem",
+            help="SSL certificate file")
+    parser.add_option("--key", default=None,
+            help="SSL key file (if separate from cert)")
+    parser.add_option("--ssl-only", action="store_true",
+            help="disallow non-encrypted connections")
+    (opts, args) = parser.parse_args()
     hostname = 'localhost'
     port = '8080'
-    w=WebsocketRenderer(hostname,'index.html',port,8000)
+    
+    if len(args) == 0:
+       opts.listen_port = 8000
+    else:
+       opts.listen_port = int(args[0])
+
+    opts.web = './static/'  #changes directory to here
+    print opts.__dict__
+    
+    w=RenderThread(opts)
     s=SimpleWebserver(hostname,port)
     #webbrowser.open('http://'+hostname+':'+str(port))
     while True:
